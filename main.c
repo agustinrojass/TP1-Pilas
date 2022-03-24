be7235077688fcc617b8de2bdafabbb065ed7b02
Agustin Rojas <agustinrojas259@gmail.com>
2022-03-24 00:35:13 -0300

    Version 1.0

---
+++ main.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ps();
+int pcss();
+int pcsc();
+int pcr();
+int main()
+{
+    int ejercicio;
+    do
+    {
+        do
+        {
+            printf("\nEJERCICIOS                                                 INGRESAR");
+            printf("\nEJERCICIO 1  ...................................................  1");
+            printf("\nEJERCICIO 2  ...................................................  2");
+            printf("\nEJERCICIO 3  ...................................................  3");
+            printf("\nEJERCICIO 4  ...................................................  4");
+            printf("\nEJERCICIO 5  ...................................................  5");
+            printf("\nEJERCICIO 6  ...................................................  6");
+            printf("\nEJERCICIO 7  ...................................................  7");
+            printf("\nEJERCICIO 8  ...................................................  8");
+            printf("\nEJERCICIO 9  ...................................................  9");
+            printf("\nEJERCICIO 10 ................................................... 10");
+            printf("\nEJERCICIO 11 ................................................... 11");
+            printf("\nEJERCICIO 12 ................................................... 12");
+            printf("\nEJERCICIO 13 ................................................... 13");
+            printf("\nEJERCICIO 14 ................................................... 14");
+            printf("\nEJERCICIO 15 ................................................... 15");
+            printf("\nEJERCICIO 16 ................................................... 16");
+            printf("\nEJERCICIO 17 ................................................... 17");
+            printf("\nEJERCICIO 18 ................................................... 18\n");
+            printf("\nIngrese el ejercicio: ");
+            scanf("%i",&ejercicio);
+        }
+        while(ejercicio!=1 && ejercicio!=2 && ejercicio!=3 && ejercicio!=4 && ejercicio!=5 && ejercicio!=6 && ejercicio!=7 && ejercicio!=8 && ejercicio!=9 && ejercicio!=10 && ejercicio!=11 && ejercicio!=12 && ejercicio!=13 && ejercicio!=14 && ejercicio!=15 && ejercicio!=16 && ejercicio!=17 && ejercicio!=18 && ejercicio!=0);
+        switch(ejercicio)
+        {
+            case 1:
+            {
+
+            }
+            break;
+            case 2:
+            {
+
+            }
+            break;
+            case 3:
+            {
+
+            }
+            break;
+            case 4:
+            {
+
+            }
+            break;
+            case 5:
+            {
+
+            }
+            break;
+            case 6:
+            {
+
+            }
+            break;
+            case 7:
+            {
+
+            }
+            break;
+            case 8:
+            {
+
+            }
+            break;
+            case 9:
+            {
+
+            }
+            break;
+            case 10:
+            {
+
+            }
+            break;
+            case 11:
+            {
+
+            }
+            break;
+            case 12:
+            {
+
+            }
+            break;
+            case 13:
+            {
+
+            }
+            break;
+            case 14:
+            {
+
+            }
+            break;
+            case 15:
+            {
+
+            }
+            break;
+            case 16:
+            {
+
+            }
+            break;
+            case 17:
+            {
+
+            }
+            break;
+            case 18:
+            {
+
+            }
+            break;
+        }
+    }
+    while(ejercicio!=0);
+    printf("\nTP1 TERMINADO\n");
+    printf("\nVersion 1.0\n");
+    return 0;
+}
